371d3de9aac2ec8d4f1c6032bbe7c109eed3c1ca
merkost <konstantinlikes@gmail.com>
2023-06-23 18:43:00 +1000

    Created weather_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,56 @@
 import 'package:first_proj/pages/home_page.dart';
-import "package:flutter/material.dart";
-import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'package:first_proj/screens/weather_screen.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  debugPaintSizeEnabled = false;
-  return runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quizeeee',
+      title: 'My App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        textTheme: TextTheme(
-          caption: TextStyle(fontSize: 22.0, color: Colors.white),
-        ),
-        fontFamily: 'Georgia',
       ),
-      home: HomePage(),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Weather'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => WeatherPage()),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Quiz'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/weather_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+import '../db/entity/weather_entity.dart';
+import '../repository/weather_repository.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData.dark(),
+      home: WeatherPage(),
+    );
+  }
+}
+
+class WeatherPage extends StatefulWidget {
+  @override
+  _WeatherPageState createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final repository = WeatherRepository();
+  late Future<Weather> futureWeather;
+  bool isReloading = false; // Add a loading flag
+
+  @override
+  void initState() {
+    super.initState();
+    futureWeather = Future.delayed(Duration(seconds: 1), () {
+      return repository.getWeather();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather'),
+      ),
+      body: FutureBuilder<Weather>(
+        future: futureWeather,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Error: ${snapshot.error}'),
+                  ElevatedButton(
+                    onPressed: _refreshWeather,
+                    child: Text('Retry'),
+                  ),
+                ],
+              ),
+            );
+          } else if (snapshot.hasData) {
+            final weather = snapshot.data!;
+
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${weather.temperature}°C',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                  Image.network(weather.iconUrl),
+                  Text(
+                    weather.description,
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  Text(
+                    weather.locationName,
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(), // Show loading
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: isReloading ? null : _refreshWeather, // Disable click while loading
+        child: isReloading ? CircularProgressIndicator() : Icon(Icons.refresh), // Show loading icon if isLoading is true
+      ),
+    );
+  }
+
+  void _refreshWeather() {
+    setState(() {
+      isReloading = true; // Set loading flag to true
+    });
+
+    futureWeather = Future.delayed(Duration(seconds: 1), () {
+      return repository.updateWeather();
+    });
+
+    futureWeather.then((weather) {
+      setState(() {
+        isReloading = false;
+      });
+    }).catchError((error) {
+      setState(() {
+        isReloading = false;
+      });
+    });
+  }
+}
